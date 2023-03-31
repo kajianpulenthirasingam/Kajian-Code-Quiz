a17c917f793164a2ade0a9f36f7d5db781eeb1b8
Kajiane <kajianpulenthirasingam@gmail.comm>
2023-03-30 22:26:12 -0400

    Fixed displaying issues and formatted the questions into the javascript

--- script.js
+++ script.js
@@ -1,159 +1,121 @@
-// Define the questions, answers, and choices
-var questions = [
+var startButton = document.getElementById("start-button");
+var quizContainer = document.getElementById("quiz-container");
+var scoreContainer = document.getElementById("score-container");
+var questionElement = document.getElementById("question");
+var choiceButtons = document.querySelectorAll(".choice");
+var timeElement = document.getElementById("time");
+var scoreElement = document.getElementById("score");
+var initialsElement = document.getElementById("initials");
+var highscoresElement = document.getElementById("highscores");
+var QUIZ_TIME = 60; // in seconds
+var SCORE_INCREMENT = 10;
+var currentQuestionIndex;
+var score;
+var quizTime;
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  startButton.style.display = "none";
+  quizContainer.style.display = "block";
+  scoreContainer.style.display = "none";
+  currentQuestionIndex = 0;
+  score = 0;
+  quizTime = QUIZ_TIME;
+  displayQuestion();
+  startTimer();
+}
+
+// Define an array of objects that contain each question and its choices
+var quizQuestions = [
   {
-    question: "What is the result of 2 + 2?",
-    choices: ["3", "4", "5", "6"],
-    answer: "4",
+    question: "Arrays in JavaScript is used to store?",
+    choices: ["Number", "String", "Boolean", "All of the above"],
+    answerIndex: 3,
   },
   {
-    question: "What is the result of 10 / 2?",
-    choices: ["2", "5", "10", "20"],
-    answer: "5",
+    question: "Commonly used Data types DO NOT INCLUDE?",
+    choices: ["String", "Boolean", "Alerts", "Numbers"],
+    answerIndex: 2,
   },
   {
-    question: "What is the result of 5 * 3?",
-    choices: ["8", "12", "15", "18"],
-    answer: "15",
-  },
-  {
-    question: "What is the result of 20 - 8?",
-    choices: ["10", "12", "15", "18"],
-    answer: "12",
+    question: "The condition in an if / else statement is enclosed within ___?",
+    choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+    answerIndex: 2,
   },
 ];
 
-// Define the time for each question, and the penalty for a wrong answer
-var timePerQuestion = 15; // seconds
-var timePenalty = 10; // seconds
+// Get the quiz elements
+var questionEl = document.querySelector("h3");
+var choicesEl = document.querySelector(".choices");
+var submitBtn = document.querySelector(".submit");
 
-// Define variables for the quiz state
-var timeLeft = 0;
-var score = 0;
-var questionIndex = 0;
-var timerInterval;
-
-// Get the elements from the HTML
-var timerEl = document.getElementById("time");
-var highscoresEl = document.getElementById("highscores");
-var nameEl = document.getElementById("name");
-var startButtonEl = document.getElementById("start-button");
-var quizContainerEl = document.getElementById("quiz-container");
-var questionEl = document.getElementById("question");
-var choicesEl = document.getElementById("choices");
-var scoreContainerEl = document.getElementById("score-container");
-var scoreEl = document.getElementById("score");
-
-// Add an event listener to the start button
-startButtonEl.addEventListener("click", startQuiz);
-
-// Define the startQuiz function
-function startQuiz() {
-  // Hide the start button and highscores
-  startButtonEl.style.display = "none";
-  highscoresEl.style.display = "none";
-
-  // Show the quiz
-  quizContainerEl.style.display = "block";
-
-  // Reset the quiz state
-  timeLeft = questions.length * timePerQuestion;
-  score = 0;
-  questionIndex = 0;
-
-  // Start the timer
-  setTimer();
-
-  // Display the first question
-  displayQuestion();
+// Show the current question and choices
+function displayQuestion() {
+  var currentQuestion = quizQuestions[currentQuestionIndex];
+  questionElement.innerText = currentQuestion.question;
+  choiceButtons.forEach(button => {
+    var choice = currentQuestion.choices[button.dataset.index];
+    button.innerText = choice;
+    button.dataset.correct = choice === currentQuestion.choices[currentQuestion.answerIndex];
+    button.addEventListener("click", handleChoiceClick);
+  });
 }
 
-
-// Define the setTimer function
-function setTimer() {
-  // Update the timer display
-  timerEl.textContent = timeLeft;
-
-  // Set an interval to update the timer every second
-  timerInterval = setInterval(function() {
-    // Decrement the time left
-    timeLeft--;
-
-    // Update the timer display
-    timerEl.textContent = timeLeft;
-
-    // If the time runs out, end the quiz
-    if (timeLeft <= 0) {
-      clearInterval(timerInterval);
+function handleChoiceClick(event) {
+  var selectedButton = event.target;
+  var isCorrect = selectedButton.dataset.correct === "true";
+  if (isCorrect) {
+    score += SCORE_INCREMENT;
+  } else {
+    quizTime -= 5; // penalty for wrong answer
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quizQuestions.length) {
+    displayQuestion();
+  } else {
+    endQuiz();
+  }
+}
+//Starts the timer and decreases every second
+function startTimer() {
+  var timerId = setInterval(() => {
+    quizTime--;
+    timeElement.innerText = quizTime;
+    if (quizTime <= 0) {
+      clearInterval(timerId);
       endQuiz();
     }
   }, 1000);
 }
-
+//Ends the quiz
 function endQuiz() {
-  // Stop the timer
-  clearInterval(timerInterval);
-
-  // Hide the quiz container
-  quizContainerEl.style.display = "none";
-
-  // Show the score container
-  scoreContainerEl.style.display = "block";
-
-  // Display the final score
-  scoreEl.textContent = score;
-}
-// Define the displayQuestion function
-function displayQuestion() {
-  // Get the current question object
-  var currentQuestion = questions[questionIndex];
-
-  // Display the question text
-  questionEl.textContent = currentQuestion.question;
-
-  // Remove any existing choices
-  choicesEl.innerHTML = "";
-
-  // Loop through the choices and create a button for each one
-  for (let i = 0; i < currentQuestion.choices.length; i++) {
-    // Create a new button element
-    var choiceButton = document.createElement("button");
-  
-    // Set the button text to the current choice
-    choiceButton.textContent = currentQuestion.choices[i];
-
-    // Add the choice to the button element
-    choicesEl.appendChild(choiceButton);
-
-    // Attach an event listener to the button that handles when the user selects the choice
-    choiceButton.addEventListener("click", function() {
-      // Check if the user's choice is correct
-      if (currentQuestion.answer === i) {
-        // Increase the score by 1
-        score++;
-
-        // Display the updated score
-        scoreEl.textContent = score;
-
-        // Display a message indicating that the answer is correct
-        messageEl.textContent = "Correct!";
-      } else {
-        // Display a message indicating that the answer is incorrect
-        messageEl.textContent = "Sorry, that's incorrect.";
-        
-        // Deduct time for wrong answer
-        timeLeft -= timePenalty;
-        
-        // Update the timer display
-        timerEl.textContent = timeLeft;
-      }
-
-      // Move on to the next question, or end the quiz if there are no more questions
-      questionIndex++;
-      if (questionIndex >= questions.length) {
-        endQuiz();
-      } else {
-        displayQuestion();
-      }
-    });
+  quizContainer.style.display = "none";
+  scoreContainer.style.display = "block";
+  scoreElement.innerText = score;
+  quizTime = 0;
+  if (quizTime != 0){
+    quizTime = 0;
   }
 }
+
+function saveScore(event) {
+  event.preventDefault();
+  var initials = initialsElement.value;
+  var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+  highscores.push({ initials, score });
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+  displayHighscores();
+}
+
+function displayHighscores() {
+  var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+  highscoresElement.innerHTML = "";
+  highscores.forEach((highscore, index) => {
+    var scoreElement = document.createElement("div");
+    scoreElement.innerText = `${index + 1}. ${highscore.initials} - ${highscore.score}`;
+    highscoresElement.appendChild(scoreElement);
+  });
+}
+
+document.forms[0].addEventListener("submit", saveScore);
